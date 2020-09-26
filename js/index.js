95d73c9116b57139fe7c96909a075bfe714d5254
Ritikrai09 <62825561+Ritikrai09@users.noreply.github.com>
2020-09-26 23:00:22 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,35 +1,7 @@
 var data11=[];
 var data12=[];
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
 
-slider.oninput = function() {
-    var x=this.value;
-  output.innerHTML = this.value;
-
-  document.getElementById("displacement").value=x;
- var  trans = 'rotate(89.6352767944336 282.1059265136719,226.01145935058594)translate('+0+','+-this.value+')';
- var trans1 = 'translate('+this.value+','+0+')';
- var trans2 = 'rotate(88.92388916015625 357.9019775390625,227.59671020507812) translate('+0+','+-this.value+')'; 
-  document.getElementById("svg_44").setAttribute("transform",trans);
-   document.getElementById("svg_2").setAttribute("transform",trans1);
-   document.getElementById("svg_5").setAttribute("transform",trans2);
-
-   if (this.value > 0) {
-
-   	 document.getElementById("myRange").style.background = 'linear-gradient(to left,cornflowerblue 50%,transparent 50%)';
-
-   }
-
-    if (this.value < 0) {
-
-   	 document.getElementById("myRange").style.background = 'linear-gradient(to right,red 50%,transparent 50%)';
-
-   }
-  
-}
 
 function response(){
     const suppfre=document.getElementById("frequency").value;
@@ -48,11 +20,65 @@ const f=roundoffR2.toFixed(4);
 data11.push(f);
 document.getElementById("vout").value=f;
 
-var element = document.getElementById("div2").innerHTML+="Vout.= "+ f + ", "+"Displacement= "+suppdis+"<br>";
-
+var addup = document.querySelector("#output").insertRow(2);
+var add1 = addup.insertCell(0).innerHTML= f;
+var add2 = addup.insertCell(1).innerHTML=suppdis;
 }
 
 function reset(){
     document.getElementById("myForm").reset();
     document.getElementById("myRange").reset();
 }
+
+document.querySelector(".arrow1").style.display = 'none';
+document.querySelector(".arrow2").style.display = 'none';
+
+document.querySelector('.switching').onclick=function(){
+
+  document.querySelector('.switching').style.backgroundColor='yellow';
+  document.querySelector('.switching').style.boxShadow='0px -2px 20px 0px inset #444';
+    document.querySelector('.switching').style.transform='scale('+0.8+'deg)';
+   document.querySelector('.arrow').style.display='block';
+     document.querySelector('#svg_51').style.display='block';
+      document.querySelector('#svg_13').style.display='block';
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    var x=this.value;
+  output.innerHTML = this.value;
+
+  document.getElementById("displacement").value=x;
+ var  trans = 'rotate(89.6352767944336 282.1059265136719,226.01145935058594)translate('+0+','+-this.value+')';
+ var trans1 = 'translate('+this.value+','+0+')';
+ var trans2 = 'rotate(88.92388916015625 357.9019775390625,227.59671020507812) translate('+0+','+-this.value+')'; 
+  document.getElementById("svg_44").setAttribute("transform",trans);
+   document.getElementById("svg_2").setAttribute("transform",trans1);
+   document.getElementById("svg_5").setAttribute("transform",trans2);
+
+   if (this.value > 0) {
+
+     document.getElementById("myRange").style.background = 'linear-gradient(to left,cornflowerblue 50%,transparent 50%)';
+      document.querySelector(".arrow1").style.display = 'block'; 
+       document.querySelector(".arrow2").style.display = 'block'; 
+   }
+
+ if (this.value == 0) {
+
+     document.querySelector(".arrow1").style.display = 'none'; 
+      document.querySelector(".arrow2").style.display = 'none'; 
+   }
+
+    if (this.value < 0) {
+
+     document.getElementById("myRange").style.background = 'linear-gradient(to right,red 50%,transparent 50%)';
+     document.querySelector(".arrow1").style.display = 'block'; 
+      document.querySelector(".arrow2").style.display = 'block'; 
+   }
+  
+}
+
+
+}           
